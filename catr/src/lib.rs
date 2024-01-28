ce902e04dae1d4cbfd3ca44ba7cd595d679dd7b6
tatenodev <gd28.c.tateno@gmail.com>
2024-01-28 13:08:53 +0900

    add open

--- catr/src/lib.rs
+++ catr/src/lib.rs
@@ -1,11 +1,21 @@
-use std::error::Error;
-
 use clap::{App, Arg};
+use std::error::Error;
+use std::fs::File;
+use std::io::{self, BufRead, BufReader};
 
 type MyResult<T> = Result<T, Box<dyn Error>>;
 
+fn open(filename: &str) -> MyResult<Box<dyn BufRead>>{
+  match filename {
+    "-" => Ok(Box::new(BufReader::new(io::stdin()))),
+    _ => Ok(Box::new(BufReader::new(File::open(filename)?))),
+  }
+}
+
 pub fn run(config: Config) -> MyResult<()> {
-  dbg!(config);
+  for filename in config.files {
+    println!("{}", filename);
+  }
   Ok(())
 }
 
