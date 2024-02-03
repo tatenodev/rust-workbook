f7df3ca53e6f068f68ec4b9e9135acb0b8314cf2
tatenodev <gd28.c.tateno@gmail.com>
2024-02-03 20:05:30 +0900

    cargo fmt

--- catr/src/lib.rs
+++ catr/src/lib.rs
@@ -1,70 +1,71 @@
 use clap::{App, Arg};
 use std::error::Error;
-use std::fs::{read_to_string, File};
+use std::fs::{File};
 use std::io::{self, BufRead, BufReader};
 
 type MyResult<T> = Result<T, Box<dyn Error>>;
 
-fn open(filename: &str) -> MyResult<Box<dyn BufRead>>{
-  match filename {
-    "-" => Ok(Box::new(BufReader::new(io::stdin()))),
-    _ => Ok(Box::new(BufReader::new(File::open(filename)?))),
-  }
+fn open(filename: &str) -> MyResult<Box<dyn BufRead>> {
+    match filename {
+        "-" => Ok(Box::new(BufReader::new(io::stdin()))),
+        _ => Ok(Box::new(BufReader::new(File::open(filename)?))),
+    }
 }
 
 pub fn run(config: Config) -> MyResult<()> {
-  for filename in config.files {
-    match open(&filename) {
-        Err(err) => eprintln!("{}: {}", filename, err),
-        Ok(file) => {
-          for line_result in file.lines() {
-            let line = line_result?;
-            println!("{}", line);
-          }
+    for filename in config.files {
+        match open(&filename) {
+            Err(err) => eprintln!("{}: {}", filename, err),
+            Ok(file) => {
+                for line_result in file.lines() {
+                    let line = line_result?;
+                    println!("{}", line);
+                }
+            }
         }
     }
-  }
-  Ok(())
+    Ok(())
 }
 
 #[derive(Debug)]
 pub struct Config {
-  files: Vec<String>,
-  number_lines: bool,
-  number_nonblank_lines: bool,
+    files: Vec<String>,
+    number_lines: bool,
+    number_nonblank_lines: bool,
 }
 
 pub fn get_args() -> MyResult<Config> {
-  let matches = App::new("catr")
-    .version("0.1.0")
-    .author("tatenodev <@tatenodev>")
-    .about("Rust cat")
-    .arg(
-      Arg::with_name("files")
-        .value_name("FILES")
-        .help("Input file path")
-        .multiple(true)
-        .default_value("-")
-    )
-    .arg(
-      Arg::with_name("number")
-         .short("n")
-         .long("number")
-         .help("Number")
-         .takes_value(false)
-         .conflicts_with("number_nonblank")
-    )
-    .arg(
-      Arg::with_name("number_nonblank")
-      .short("b")
-      .long("number-nonblank")
-      .help("Number nonblank")
-      .takes_value(false)
-    ).get_matches();
-  
-  Ok(Config {
-    files: matches.values_of_lossy("files").unwrap(),
-    number_lines: matches.is_present("number"),
-    number_nonblank_lines: matches.is_present("number_nonblank"),
-  })
+    let matches = App::new("catr")
+        .version("0.1.0")
+        .author("tatenodev <@tatenodev>")
+        .about("Rust cat")
+        .arg(
+            Arg::with_name("files")
+                .value_name("FILES")
+                .help("Input file path")
+                .multiple(true)
+                .default_value("-"),
+        )
+        .arg(
+            Arg::with_name("number")
+                .short("n")
+                .long("number")
+                .help("Number")
+                .takes_value(false)
+                .conflicts_with("number_nonblank"),
+        )
+        .arg(
+            Arg::with_name("number_nonblank")
+                .short("b")
+                .long("number-nonblank")
+                .help("Number nonblank")
+                .takes_value(false),
+        )
+        .get_matches();
+
+    Ok(Config {
+        files: matches.values_of_lossy("files").unwrap(),
+        number_lines: matches.is_present("number"),
+        number_nonblank_lines: matches.is_present("number_nonblank"),
+    })
 }
